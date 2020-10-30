99bcba665af75e7a66a4c07475d66ba79b842db1
Rami Abukhader <rami.khaderr@gmail.com>
2020-10-30 23:04:19 +0100

    OS Directories
    OS module is mainly used to create a directories and subdirectories, delete directories,  giving permissions to read or write files inside directories, creating a path for directories,

--- Modules/Module2-OS.py
+++ Modules/Module2-OS.py
@@ -1,1 +1,19 @@
-import os
+import os, stat
+
+# os.makedirs("./directory1/subdirectory1")
+# os.makedirs("./directory1/subdirectory2")
+
+# print(os.getcwd()) #we get the current working directories
+
+#removing the directory
+
+# path = os.getcwd()
+# os.rmdir("./directory1/subdirectory1")
+
+# os.chmod("./directory1/subdirectory2/test.txt", stat.S_IREAD) # giving a permision to the directory and read only by the owner
+#  we can stat.S_IROTH mode to make it acceble by everyone, or IWROTE that can be written by only owner, or IXUSER by everyone
+# print("File can be raed only by owner")
+
+print(os.path.join('a', 'b', 'c')) # creating bath for directories
+
+
