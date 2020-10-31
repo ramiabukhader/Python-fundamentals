6ea457e2bdb81b8dc7c460868cc6a7ae597dd8c2
Rami Abukhader <rami.khaderr@gmail.com>
2020-10-31 19:16:12 +0100

    web-scraping (web harvesting) first demo
    Web scraping is way to collect information from websites, for example we want the products and its priceses listed in a websit, web scraping helps us to get all of those by a couple of lines of code.

---
+++ Web-Scraping/WebScraping-demo.py
@@ -0,0 +1,11 @@
+# we want to scrape the content of the demo file i've created to make a better understanding of the webscaraping
+from bs4 import BeautifulSoup
+
+raw_html = open('demo.html').read()
+
+html = BeautifulSoup(raw_html, 'html.parser') #read about parser for better understanding
+
+for p in html.select('p'): # selected the paragraph tag
+    # print(p.text)
+    if p['id'] =='first':
+        print(p.text)
